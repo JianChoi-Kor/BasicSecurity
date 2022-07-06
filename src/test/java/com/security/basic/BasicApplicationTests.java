72ea8c9117216ffd4108dcaa3effb210118c90a8
JianChoi-Kor <wldks79@gmail.com>
2022-07-06 21:49:29 +0900

    start

---
+++ src/test/java/com/security/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
